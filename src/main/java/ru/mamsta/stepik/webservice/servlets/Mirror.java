4aceebf4c82f965fdf688771910e78d5537a5b56
st-slav <st.mam_@hotmail.com>
2017-11-24 20:06:20 +0500

    it's beautiful

--- src/main/java/ru/mamsta/stepik/webservice/servlets/Mirror.java
+++ src/main/java/ru/mamsta/stepik/webservice/servlets/Mirror.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 public class Mirror extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
-        //response.setContentType("text/html;charset=utf-8");
+        response.setContentType("text/html;charset=utf-8");
         try {
             response.getWriter().println(request.getParameter("key"));
-            //response.setStatus(HttpServletResponse.SC_OK);
+            response.setStatus(HttpServletResponse.SC_OK);
         } catch (IOException ex) {
             response.setStatus(HttpServletResponse.SC_NO_CONTENT);
         }
